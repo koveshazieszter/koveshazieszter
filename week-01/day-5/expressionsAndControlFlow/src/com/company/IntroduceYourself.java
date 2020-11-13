5265871e894e5886a8de120a1f6a3df123dd7b0c
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-13 15:02:32 +0100

    I added a file that introduces me.

---
+++ week-01/day-5/expressionsAndControlFlow/src/com/company/IntroduceYourself.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class IntroduceYourself {
+    public static void main(String[] args){
+        System.out.println("Eszter Kövesházi");
+        System.out.println(30);
+        System.out.println(1.60);
+    }
+}
