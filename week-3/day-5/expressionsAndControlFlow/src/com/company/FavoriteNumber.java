a8116514ed441cf93e66278e2dc6be46445c2a97
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-13 16:19:34 +0100

    Change the folder name to week-3.

---
+++ week-3/day-5/expressionsAndControlFlow/src/com/company/FavoriteNumber.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 6;
+        System.out.println("A kedvenc számom: " + favoriteNumber);
+    }
+}
---
+++ week-3/day-5/expressionsAndControlFlow/src/com/company/LipemLopom.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class LipemLopom{
+        public static void main(String[] args)
+
+            {
+                System.out.println("elaludt az öreg csősz.");
+                System.out.println("Lipem, lopom a szőlőt,");
+                System.out.println("vaskalap a fejébe.");
+                System.out.println("Furkosbot a kezében,");
+
+            }
+        }
