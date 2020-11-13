1dc772569d57e0edaf79c4319ead336d92343ef2
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-13 19:09:55 +0100

    Folder remove week-3

--- week-3/day-5/expressionsAndControlFlow/src/com/company/FavoriteNumber.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class FavoriteNumber {
-    public static void main(String[] args) {
-        int favoriteNumber = 6;
-        System.out.println("A kedvenc számom: " + favoriteNumber);
-    }
-}
--- week-3/day-5/expressionsAndControlFlow/src/com/company/HelloOthers.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class HelloOthers {
-    public static void main(String[] args) {
-        System.out.println("Helló Gyula !");
-        System.out.println("Helló Johanna !");
-        System.out.println("Helló Daniel !");
-    }
-}
