07111901c95f1088a471d92b722e31cfe974287c
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-14 22:05:53 +0100

    Print even number

---
+++ week-03/day-5/expressionsAndControlFlow/src/com/company/PrintEven.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class PrintEven {
+    public static void main(String[] args) {
+        int i=0;
+        while( i<=500){
+
+            if (i%2==0){
+                System.out.println(i);
+
+            }
+            i++;
+        }
+    }
+}
