461561328b965afd3931604fe54e68b22e782447
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-14 21:51:30 +0100

    exam cheating for loop

---
+++ week-03/day-5/expressionsAndControlFlow/src/com/company/IWontCheatOnTheExams.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I’m not going to cheat on that exam!");
+        }
+    }
+}
