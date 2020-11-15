fdb4830abbc85deac40ab040a4998c7b6cd43e55
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-16 00:12:16 +0100

    Create a two dimensional array

---
+++ week-04/day-1/Arrays/src/com/company/Colors.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Colors {
+    public static void main(String[] args) {
+        String[][] colors = {
+                {"lime", "forest green", "olive", "pale green", "spring green"},
+                {"orange red", "red", "tomato"},
+                {"orchid", "violet", "pink", "hot pink"}
+        };
+    }
+
+}
