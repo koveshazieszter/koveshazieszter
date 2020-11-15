0fe8220aa457abf64717bd33d8a541b039452eb3
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-15 14:08:26 +0100

    Draw square

---
+++ week-03/day-5/expressionsAndControlFlow/src/com/company/DrawSquare.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter a number!");
+        int square = scanner.nextInt();
+        int spaceCount;
+        spaceCount = square;
+        for (int i = 0; i < square; i++) {
+            System.out.print("%");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < square - 2; i++) {
+            for (int j = 0; j < spaceCount; j++) {
+                if (j == 0 || j == (square - 1)) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+        for (int i = 0; i < square; i++) {
+            System.out.print("%");
+        }
+    }
+}
+
