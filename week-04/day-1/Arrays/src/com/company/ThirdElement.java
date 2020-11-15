318a5bb77cfe1540b3b0c343a87b644798c1e6f2
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-15 21:47:09 +0100

    Create an array proyect and add a new file that will find the third element in this array.

---
+++ week-04/day-1/Arrays/src/com/company/ThirdElement.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ThirdElement {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int[] numbers = {4, 5, 6, 7};
+        System.out.println(numbers[2]);
+
+    }
+}
