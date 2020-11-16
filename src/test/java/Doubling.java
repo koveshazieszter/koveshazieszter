ef3d00bee5c8a7724d418db6fabb5b6f3eac19b5
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-16 23:01:00 +0100

    Add a function that doubles a number.

--- src/test/java/Doubling.java
+++ src/test/java/Doubling.java
@@ -1,2 +1,10 @@
 public class Doubling {
+    public static void main(String[] args) {
+        System.out.println(doubling(123));
+    }
+
+    public static int doubling(int baseNum) {
+        return baseNum * 2;
+    }
 }
+
