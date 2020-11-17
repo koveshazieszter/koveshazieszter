fdf36eb6879e9467b8c90c2aa14dc34086338635
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-17 15:21:04 +0100

    Adding Greet file

--- src/test/java/com/company/Greet.java
+++ src/test/java/com/company/Greet.java
@@ -2,13 +2,13 @@ package com.company;
 
 public class Greet {
     public static void main(String[] args) {
-        
- // - Hozz létre egy class-t `Greet` névvel és benne a main függvényt.
-// - Hozz létre egy `name` nevű String változót, aminek az értéke legyen `Green Fox`.
-// - Készíts egy függvényt `greet` névvel mely üdvözli a bemeneti paraméterét kiírással(void & print)
-//   pl.: `Üdv, kedves Green Fox`
-// - greet(`name`)
 
+        String name = "Green Fox";
+        greet(name);
+    }
+    static void greet(String name) {
 
+        System.out.println("Üdv, kedves " + name);
     }
 }
+
