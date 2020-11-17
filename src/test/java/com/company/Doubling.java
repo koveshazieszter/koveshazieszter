781f362c6d27b7b6382666fba8df1b9d786be3fe
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-17 14:48:22 +0100

    add a pckage to file

--- src/test/java/com/company/Doubling.java
+++ src/test/java/com/company/Doubling.java
@@ -1,3 +1,5 @@
+package com.company;
+
 public class Doubling {
     public static void main(String[] args) {
         System.out.println(doubling(123));
---
+++ src/test/java/com/company/Greet.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Greet {
+    public static void main(String[] args) {
+        
+ // - Hozz létre egy class-t `Greet` névvel és benne a main függvényt.
+// - Hozz létre egy `name` nevű String változót, aminek az értéke legyen `Green Fox`.
+// - Készíts egy függvényt `greet` névvel mely üdvözli a bemeneti paraméterét kiírással(void & print)
+//   pl.: `Üdv, kedves Green Fox`
+// - greet(`name`)
+
+
+    }
+}
