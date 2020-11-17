e19b3232e0c43907424478a7ee83f2beb249c2b9
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-17 15:21:43 +0100

    src/test/java/com/company/Doubling.java

--- src/test/java/com/company/Doubling.java
+++ src/test/java/com/company/Doubling.java
@@ -2,10 +2,12 @@ package com.company;
 
 public class Doubling {
     public static void main(String[] args) {
+
         System.out.println(doubling(123));
     }
 
     public static int doubling(int baseNum) {
+
         return baseNum * 2;
     }
 }
