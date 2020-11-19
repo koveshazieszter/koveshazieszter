6f6e090ecd95d4994839fd60c999ffcb59a80d25
Kövesházi Eszter <koveshazieszter@gmail.com>
2020-11-19 22:01:24 +0100

    i created a new project string manipulation string manipulation i do in it.

---
+++ week-04/day-5/String/src/com/company/SimpleReplace.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class SimpleReplace {
+
+    public static void main(String[] args) {
+
+        String example = "In a dishwasher far far away";
+        String galaxy=example.replace("dishwasher", "galaxy");
+        System.out.println(galaxy);
+    }
+}
