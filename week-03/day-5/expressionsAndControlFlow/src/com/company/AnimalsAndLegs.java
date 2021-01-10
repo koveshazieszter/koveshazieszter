6e88ead53bd814c79ecd0a851f7f3a3c6a65d83f
Kövesházi Eszter <koveshazieszter@gmail.com>
2021-01-10 22:45:49 +0100

    fixing indentation

--- week-03/day-5/expressionsAndControlFlow/src/com/company/AnimalsAndLegs.java
+++ week-03/day-5/expressionsAndControlFlow/src/com/company/AnimalsAndLegs.java
@@ -3,17 +3,13 @@ package com.company;
 import java.util.Scanner;
 
 public class AnimalsAndLegs {
-    public static void main(String[] args){
-
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
         System.out.println("How many hens are on the farm?");
-    int hansOnTheFarm = scanner.nextInt();
+		int hansOnTheFarm = scanner.nextInt();
         System.out.println("How many pigs are on the farm?");
-    int pigsOnTheFarm = scanner.nextInt();
-    int animalsLegsOnTheFarm = (hansOnTheFarm*2 + pigsOnTheFarm*4);
+		int pigsOnTheFarm = scanner.nextInt();
+		int animalsLegsOnTheFarm = (hansOnTheFarm*2 + pigsOnTheFarm*4);
         System.out.println("That's the number of feet of your hens and pigs:" + animalsLegsOnTheFarm);
-
-
-
-     }
+	}
 }
